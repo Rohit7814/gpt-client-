7a04b541372b7d18788b005c90310ca0d46a2236
Rohit K Nanda <108716973+Rohit7814@users.noreply.github.com>
2023-12-01 19:14:22 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse =  async(chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('https://gpt-server-h3u9.vercel.app/', { 
+        const response = await fetch('https://server-chatgpt-alpha.vercel.app/', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
